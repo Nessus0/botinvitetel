da4b32e2a92d0f015f96ff3f726c5cca168777c2
Nessus0 <80601363+Nessus0@users.noreply.github.com>
2022-03-12 20:40:58 +0400

    Add files via upload

---
+++ adder.py
@@ -0,0 +1,107 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
+from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
+from telethon.tl.functions.channels import InviteToChannelRequest
+import sys
+import csv
+import traceback
+import time
+import random
+
+api_id = '' #Введите api_id
+api_hash = '' #Введите api_hash id
+phone = '' #Введите номер телефона с кодом страны, пример: +79999999999
+client = TelegramClient(phone, api_id, api_hash)
+async def main():
+    await client.send_message('me', 'Привет !!!!')
+
+
+
+SLEEP_TIME_1 = 100
+SLEEP_TIME_2 = 100
+
+with client:
+    client.loop.run_until_complete(main())
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('40779'))
+
+users = []
+with open(r"Scrapped.csv", encoding='UTF-8') as f:  
+    rows = csv.reader(f,delimiter=",",lineterminator="\n")
+    next(rows, None)
+    for row in rows:
+        user = {}
+        user['username'] = row[0]
+        user['id'] = int(row[1])
+        user['access_hash'] = int(row[2])
+        user['name'] = row[3]
+        users.append(user)
+
+chats = []
+last_date = None
+chunk_size = 200
+groups = []
+
+result = client(GetDialogsRequest(
+    offset_date=last_date,
+    offset_id=0,
+    offset_peer=InputPeerEmpty(),
+    limit=chunk_size,
+    hash=0
+))
+chats.extend(result.chats)
+
+for chat in chats:
+    try:
+        if chat.megagroup == True:
+            groups.append(chat)
+    except:
+        continue
+
+print('Выберите чат:')
+i = 0
+for group in groups:
+    print(str(i) + '- ' + group.title)
+    i += 1
+
+g_index = input("Введите число: ")
+target_group = groups[int(g_index)]
+
+target_group_entity = InputPeerChannel(target_group.id, target_group.access_hash)
+
+mode = int(2)
+
+n = 0
+
+for user in users:
+    n += 1
+    if n % 80 == 0:
+        sleep(60)
+    try:
+        print("Добавляю {}".format(user['id']))
+        if mode == 1:
+            if user['username'] == "":
+                continue
+            user_to_add = client.get_input_entity(user['username'])
+        elif mode == 2:
+            user_to_add = InputPeerUser(user['id'], user['access_hash'])
+        else:
+            sys.exit("Ошибка. Повторите попытку.")
+        client(InviteToChannelRequest(target_group_entity, [user_to_add]))
+        print("Жду 60-180 секунд...")
+        time.sleep(random.randrange(60, 90))
+    except PeerFloodError:
+        print("Ошибка флуда, лучше повторить попытку позже.")
+        print("Жду {} секунд".format(SLEEP_TIME_2))
+        time.sleep(SLEEP_TIME_2)
+    except UserPrivacyRestrictedError:
+        print("Нельзя добавить участника. Пропускаю.")
+        print("Жду 5 секунд...")
+        time.sleep(random.randrange(5, 0))
+    except:
+        traceback.print_exc()
+        print("Ошибка")
+        continue
---
+++ scrapper.py
@@ -0,0 +1,75 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+import csv
+
+api_id = '' #Введите api_id
+api_hash = '' #Введите api_hash id
+phone = '' #Введите номер телефона с кодом страны, пример: +79999999999
+client = TelegramClient(phone, api_id, api_hash)
+async def main():
+    await client.send_message('me', 'Привет !!!!')
+with client:
+    client.loop.run_until_complete(main())
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Введите код подтверждения: '))
+
+
+chats = []
+last_date = None
+chunk_size = 200
+groups=[]
+
+result = client(GetDialogsRequest(
+             offset_date=last_date,
+             offset_id=0,
+             offset_peer=InputPeerEmpty(),
+             limit=chunk_size,
+             hash = 0
+         ))
+chats.extend(result.chats)
+
+
+
+for chat in chats:
+    try:
+        if chat.megagroup== True:
+            groups.append(chat)
+    except:
+        continue
+
+print('Выберите чат, из которого хотите получить список участников:')
+i=0
+for g in groups:
+    print(str(i) + '- ' + g.title)
+    i+=1
+
+g_index = input("Введите число: ")
+target_group=groups[int(g_index)]
+
+print('Получаю участников...')
+all_participants = []
+all_participants = client.get_participants(target_group, aggressive=False)
+
+print('Сохраняю в файл...')
+with open("Scrapped.csv","w",encoding='UTF-8') as f:#Enter your file name.
+    writer = csv.writer(f,delimiter=",",lineterminator="\n")
+    writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
+    for user in all_participants:
+        if user.username:
+            username= user.username
+        else:
+            username= ""
+        if user.first_name:
+            first_name= user.first_name
+        else:
+            first_name= ""
+        if user.last_name:
+            last_name= user.last_name
+        else:
+            last_name= ""
+        name= (first_name + ' ' + last_name).strip()
+        writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])
+print('Успешно записал все данные в файл.......')
